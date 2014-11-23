3342d36b462211ba4688995c07a04414077b9b6b
Brian Ko <brianswko@gmail.com>
2014-11-22 21:21:31 -0800

    Problem 1 spec rework

--- spec/solutions/problem1_spec.rb
+++ spec/solutions/problem1_spec.rb
@@ -1,14 +1,16 @@
 require 'spec_helper'
 
-describe ProjectEuler::Problem1 do
-  describe '.sum_multiples_of_3_or_5' do
-    let(:upper_bound)       { 1000 }
-    let(:sum_multiples_3_5) { 233168 }
+module ProjectEuler
+  describe Problem1 do
+    describe '.sum_multiples_of_3_or_5' do
+      let(:upper_bound)       { 1000 }
+      let(:sum_multiples_3_5) { 233168 }
 
-    it 'outputs the correct value' do
-      expect(
-        ProjectEuler::Problem1.sum_multiples_of_3_or_5(upper_bound)
-      ).to eq(sum_multiples_3_5)
+      it 'outputs the correct value' do
+        expect(
+          Problem1.sum_multiples_of_3_or_5(upper_bound)
+        ).to eq(sum_multiples_3_5)
+      end
     end
   end
 end
