616b662ca952e7bd3b0f58d4ac2be8ddc58daa25
Brian Ko <brianswko@gmail.com>
2014-11-23 21:15:32 -0800

    Add Problem 4

--- lib/project_euler.rb
+++ lib/project_euler.rb
@@ -1,6 +1,7 @@
 require 'project_euler/solutions/problem1'
 require 'project_euler/solutions/problem2'
 require 'project_euler/solutions/problem3'
+require 'project_euler/solutions/problem4'
 
 module ProjectEuler
 end
